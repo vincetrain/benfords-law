60480b9d0dc194388ccb405c84705d8b0cb8ddfd
Vincent <vintran2015@gmail.com>
2021-05-12 19:42:43 -0400

    Edits on main() method + new methods
    Modified main() method by adding custom file support, and cleaning up the code a bit.
    
    Also added printMenu() method to print the menu in main().
    
    Added validFile() method to check if inputted file exists and is CSV.

--- BenfordsLawAssignment.java
+++ BenfordsLawAssignment.java
@@ -13,51 +13,50 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.File;
 
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.statistics.HistogramDataset;
+
 class BenfordsLawAssignment{
     public static void main(String[] args){
         Scanner reader = new Scanner(System.in);
-        // declare variables
-        String readFile;
-        String checkSales;
-        // assign a variable to the file name
-        String readCSVFile = "sales.csv";  
-        String writeCSVFile = "results.csv";   
-        // the array contains the digit frequencies 
         Double frequencyArray[] = {30.1, 17.6, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.0};
        
-        // prompt: to read, or not to read sales file 
-        System.out.print("Do you want to read the total sales amount file? (yes, or no): ");
-        readFile = reader.nextLine();
-        if (readFile.equalsIgnoreCase("yes")){
-            readFile(readCSVFile);    // call method 
-            // prompt: to analyze, or not to analyze sales file 
-            System.out.print("\nDo you want to analyze sales for possible fraud? (yes, or no): ");
-            checkSales = reader.nextLine();
-            if (checkSales.equalsIgnoreCase("yes")){
-                // method for benfords law; graph 
-                // after the validation/graph,
-                // call:
-                generateCSVFile(frequencyArray, writeCSVFile);  
+        // User input variables
+        String userInput;
+
+        // Condition variables
+        String continueCondition = "1";
+        String exitCondition = "2";
+
+        // Prompts user if not inputted, or input was invalid. Also loops the program, which is nice.
+        do {
+            printMenu();    // Prints the menu
+            reader = new Scanner(System.in);
+            userInput = reader.nextLine();
+            if (userInput.equals(continueCondition)) {
+                do {
+                    System.out.print("Input CSV file directory: ");
+                    reader = new Scanner(System.in);
+                    userInput = reader.nextLine();
+                } while (!validFile(userInput));
+                readFile(userInput);
             }
-            else{
-                System.out.println("Program End.");
-            }
-        }
-        else{
-            System.out.print("Do you want to analyze sales for possible fraud? (yes, or no): ");
-            checkSales = reader.nextLine();
-            if (checkSales.equalsIgnoreCase("yes")){
-                // method for benfords law 
-                // after the validation,
-                // call:
-                generateCSVFile(frequencyArray, writeCSVFile);
-            }
-            else{
-                System.out.println("Program End.");
-            }
-        }
-        reader.close(); 
+        } while(!(userInput.equals(exitCondition)));
+
+        System.exit(0); // Exits the program
     }
+
+    /**
+     * Prints the menu using PrintStream and string concatenation.
+     * 
+     * @author Vincent Tran
+     */
+    public static void printMenu() {
+        System.out.println("Welcome to the Benford's Law Sales Analysis Program.\n"
+        .concat("1) Determine sales fraud\n")
+        .concat("2) Quit\n"));
+    }
+
     /**
      * Description - to read sales csv file: open the file; split its data; print data 
      * using an array  
@@ -124,5 +123,43 @@ class BenfordsLawAssignment{
          *     3       7%
          *     4       4%
          */
+    }
+    /**
+     * Graphs the first 9 digits using JFreechart API
+     * 
+     * @author Vincent Tran
+     * @params salesData
+     */
+    public static void generateGraph(String[] salesData) {
+        
+    }
+
+    /**
+     * Validates file to see if it exists, or is valid file type.
+     * 
+     * @author Vincent Tran
+     * @param userInput
+     * @return
+     */
+    public static boolean validFile(String userInput) {
+        String temp = "";
+        try {
+            // Iterates through the file extension, using String.lastIndexOf()
+            for (int i = userInput.lastIndexOf("."); i < userInput.length(); i++) {
+                temp = temp + userInput.charAt(i);
+            }
+            // Checks if file extension is .csv
+            if (temp.equals(".csv")) {
+                File file = new File(userInput);
+                if (file.exists()) {    // Checks if file exists
+                    return true;
+                }
+            }
+        }
+        catch (Exception StringIndexOutOfBoundsException) {
+            // does nothing
+        }
+        System.out.println("ERROR: This file does not exist, or is not a CSV.");
+        return false;
     }
 }
