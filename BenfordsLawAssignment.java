9abfbd3c9b3f4f972ba75d2ed00582a9c6c2ad28
Vincent <vintran2015@gmail.com>
2021-05-13 02:19:15 -0400

    Added generateGraph() method
    Added generateGraph() which creates a graph with a given dataset and then outputs it into a GUI.
    
    Added createDataset() which pulls data from a double array, into a DefaultCategoryDataset variable.
    
    Added too-many-more imports.
    
    Added used APIs into dependencies folder.

--- BenfordsLawAssignment.java
+++ BenfordsLawAssignment.java
@@ -1,11 +1,13 @@
 /*
 * Date: May 12, 2021
-* Author: Serena Deng 
+* Author: Serena Deng, Vincent Tran
 * Teacher: Mr.Ho
-* Description: Reads "sales.csv" file; writes a digit frequency csv file
+* Description: Reads CSV file; writes a digit frequency csv file; creates bar graph displaying digit frequency.
 **/
 
-import java.util.Scanner;    
+import java.util.Scanner;   // used for user input
+
+// Import Statements for Reading/Writing Files
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,16 +15,26 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.File;
 
+// Import Statements for Charting
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.data.statistics.HistogramDataset;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+// Import Statements for GUI
+import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
+
+import java.text.DecimalFormat; // used for rounding
 
 class BenfordsLawAssignment{
 
     public static void main(String[] args){
-        Scanner reader = new Scanner(System.in);
-        Double frequencyArray[] = {30.1, 17.6, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.0};
-       
         // User input variables
+        Scanner reader = new Scanner(System.in);
         String userInput;
 
         // Condition variables
@@ -94,10 +106,10 @@ class BenfordsLawAssignment{
      * Description - to write results csv file: 
      * 
      * @author - Serena Deng 
-     * @param arr - used to convert from double to string array; used in a for-each loop to write csv file
+     * @param dataFrequencies - used to convert from double to string array; used in a for-each loop to write csv file
      * @param writeFile - passes the file name --> results.csv
      */
-    public static void generateCSVFile(Double[] arr, String writeFile){
+    public static void generateCSVFile(double[] dataFrequencies, String writeFile){
         // counter: fabricates the 'digit' column of the file 
         int countDigit = 0;
         // create and write csv file: 
@@ -106,9 +118,9 @@ class BenfordsLawAssignment{
             StringBuilder sb = new StringBuilder();   // StringBuilder add (builds) strings to file
             sb.append("digit" + "," + "frequency\n"); // this line is displayed once, hence outside of for loop
             // the for loop iterates through the elements of the converted string array, executing code tailored to each element 
-            for (int i = 0; i < arr.length; i++){
+            for (int i = 0; i < dataFrequencies.length; i++){
                 countDigit = countDigit+1;
-                sb.append(countDigit + "," + arr[i] + "\n"); // append strings 
+                sb.append(countDigit + "," + dataFrequencies[i] + "\n"); // append strings 
             }
 
             // print data to csv file 
@@ -132,25 +144,72 @@ class BenfordsLawAssignment{
     }
     /**
      * Graphs the first 9 digits using JFreechart API
+     * <p>
+     * Uses JFreeChart to grab data and convert it into a bar graph, which is then implemented into
+     * a JFrame application initialized as a new thread to display data. 
      * 
      * @author Vincent Tran
+     * @throws IOException
      * @params salesData
      */
-    public static void generateGraph(String[] salesData) {
-        
+    public static void generateGraph(double[] dataFrequencies) {
+        CategoryDataset dataset = createDataset(dataFrequencies);   // Calls createDataset to create a dataset from the given array.
+        JFreeChart barchart = ChartFactory.createBarChart("First-Digit Frequency in Sales", "Numbers", "Frequency", dataset, PlotOrientation.VERTICAL, true, true, false); // Creates a vertical bar chart with title, x axis, y axis, and dataset.
+        BarRenderer renderer = (BarRenderer) barchart.getCategoryPlot().getRenderer();  //  calls renderer so I can mess with the ItemMargin
+        renderer.setItemMargin(-5); // Reduces bar margin to increase thickness
+
+        // Creates a JFrame application to display the bar chart
+        SwingUtilities.invokeLater(new Runnable() { // Initializes as a new thread to run below code
+            public void run() { // runs asynchronously
+                // JFrame window settings
+                JFrame frame = new JFrame();    // creates a new frame with name frame
+                frame.setTitle("First-Digit Frequency in Sales");  // Sets window title
+                frame.setResizable(true);   // Enables resizing
+                frame.setSize(1000,500);    // Sets window size to be 1000w 500h
+                frame.setVisible(true);     // Sets visibility to true
+
+                // Adding the chart into JFrame
+                ChartPanel cp = new ChartPanel(barchart);   // initializes ChartPanel variable under cp that stores the created bar chart
+                frame.getContentPane().add(cp); // adds chart (or cp) into JFrame app
+            }
+        });
     }
+
+    /**
+     * Creates a dataset by iterating through dataFrequencies array and adding values accordingly.
+     * 
+     * @author Vincent Tran
+     * @param dataFrequencies
+     * @return
+     */
+    public static CategoryDataset createDataset(double[] dataFrequencies) {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();  // new dataset variable
+        DecimalFormat f = new DecimalFormat("0.00"); // decimal format used for rounding to the nearest tenth.
+
+        // Iterates through dataFrequencies (starting at 1, ending at 9), to pull data into the dataset.
+        for (Integer i = 1; i < dataFrequencies.length+1; i++) {    // starts at 1 and ends at length+1 just to make integers non-primative, allowing me to do .toString.
+            dataset.addValue(
+            dataFrequencies[i-1], // Sets data for each bar
+            i.toString() + ": " + f.format(dataFrequencies[i-1]) + "%", // Gives numerical value along with name in the key. Also uses f DecimalFormat variable to round the decimal.
+            i.toString()    // Sets x axis names to numerical value
+            ); 
+        }
+
+        return dataset;
+    }
+
     /**
      * Calculates fraud within inputted data by comparing frequency of first digits.
      * <p>
      * Uses iteration between for-loops to iterate between indicies in the given salesData array, 
      * and then compares char types with the ascii values of digits between 1 and 9.
      * 
-     * @param salesData
      * @author Vincent Tran
+     * @param salesData
      */
     public static void benfordsLaw(String[] salesData) {
         int[] rawFrequencies = new int[9];
-        Double[] dataFrequencies = new Double[9]; // array containing 1, 2, 3, 4, 5, 6, 7, 8, 9 place holders
+        double[] dataFrequencies = new double[9]; // array containing 1, 2, 3, 4, 5, 6, 7, 8, 9 place holders
         double sum = 0.0;    // used to keep track oaf sum of all first nums occurances.
         // for loop that iterates through all indicies within salesData
         for (int i = 0; i < salesData.length; i++) {
@@ -166,7 +225,7 @@ class BenfordsLawAssignment{
         // for loop that iterates through digits 0 and 8, to then index and print frequency percentage
         for (int i = 0; i < 9; i++) {
             dataFrequencies[i] = (rawFrequencies[i]/sum)*100;   // gets 
-            System.out.format(i + ": %.2f%%\n", dataFrequencies[i]);
+            System.out.format(i + ": %.2f%%\n", dataFrequencies[i]);    // prints rounded to nearest hundredth
         }
 
         // compares first digit frequency to see if fraudulent.
@@ -176,7 +235,7 @@ class BenfordsLawAssignment{
         else {
             System.out.println("FRAUDULENT DATA: Likely\n");
         }
-
+        generateGraph(dataFrequencies);
         generateCSVFile(dataFrequencies, "result.csv");
     }
 
